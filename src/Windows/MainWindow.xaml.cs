18ea1fcd382e38d8f6992a0c5e4b6a275294425c
HotariTobu <hotari24tools@gmail.com>
2025-03-11 08:38:14 +0900

    mv: main window

--- src/Windows/MainWindow.xaml.cs
+++ src/Windows/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TFLitePoseTrainer;
+namespace TFLitePoseTrainer.Windows;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
