43fae68b7490db5d4718fa06e3611241d2e55fd3
HotariTobu <hotari24tools@gmail.com>
2025-05-17 11:24:31 +0900

    add: global exception handler

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,8 +1,21 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace TFLitePoseTrainer;
 
 public partial class App : Application
-{}
+{
+    private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        Console.Error.WriteLine(e.Exception);
+        MessageBox.Show(e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        e.Handled = true;
+
+        if (!MainWindow.IsVisible)
+        {
+            Shutdown(1);
+        }
+    }
+}
