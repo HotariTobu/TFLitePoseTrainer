04fc0374e793ab5ad554285d084fae06fe9ffe73
HotariTobu <hotari24tools@gmail.com>
2025-06-15 16:32:24 +0900

    ref: extract skeleton item

--- src/Record/DataSource.cs
+++ src/Record/DataSource.cs
@@ -1,7 +1,6 @@
 using System.Collections.ObjectModel;
 using System.Windows.Media.Imaging;
 
-using K4AdotNet.BodyTracking;
 using K4AdotNet.Sensor;
 
 namespace TFLitePoseTrainer.Record;
@@ -97,28 +96,4 @@ internal class DataSource : SharedWPF.ViewModelBase
     }
 
     #endregion
-
-    public class SkeletonItem(BodyId bodyId, Skeleton skeleton) : SharedWPF.ViewModelBase
-    {
-        #region == BodyId ==
-
-        public readonly BodyId BodyId = bodyId;
-
-        #endregion
-        #region == Skeleton ==
-
-        private Skeleton _skeleton = skeleton;
-
-        public Skeleton Skeleton
-        {
-            get => _skeleton;
-            set
-            {
-                _skeleton = value;
-                RaisePropertyChanged(nameof(Skeleton));
-            }
-        }
-
-        #endregion
-    }
 }
